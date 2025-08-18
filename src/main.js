4a4cfc8ca24b3bbfae8bbd0513fffbd3e63a3dee
Emily Valarezo <136937944+emilybibi04@users.noreply.github.com>
2025-08-18 18:33:05 +0000

    pagina principal

---
+++ src/main.js
@@ -0,0 +1,9 @@
+console.log("🌱 ReporTerra funcionando con Vite!");
+
+window.reportarIncidente = function () {
+  alert("Formulario de reporte en construcción 🚧");
+};
+
+window.verIncidentes = function () {
+  alert("Mostrando lista de incidentes (próximamente) 🌍");
+};
